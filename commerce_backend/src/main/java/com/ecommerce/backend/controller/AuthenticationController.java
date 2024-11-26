17fe55a44f4a40bc604797bfb95c669d4d182a5f
KastFartofel <paddysock@gmailcom>
2024-11-26 19:27:07 +0100

    commit display items on page

--- commerce_backend/src/main/java/com/ecommerce/backend/controller/AuthenticationController.java
+++ commerce_backend/src/main/java/com/ecommerce/backend/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+//
 import java.util.List;
 import java.util.stream.Collectors;
 
